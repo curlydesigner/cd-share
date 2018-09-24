7db7e8a21c03813b04612a7cb6e162bcfdc7a385
Yuri Panshin <ypanshin@gmail.com>
2018-09-24 15:06:01 -0400

    Unit test and project configuration

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Build Status](https://travis-ci.org/curlydesigner/cd-shared.svg?branch=master)](https://travis-ci.org/curlydesigner/cd-shared) [![Coverage Status](https://coveralls.io/repos/github/curlydesigner/cd-shared/badge.svg)](https://coveralls.io/github/curlydesigner/cd-shared)
+# Curly Designer Shared Library 
+
+This library contains shared modules for Curly Designer projects.
+## Installation
+```sh
+npm install https://github.com/curlydesigner/cd-shared.git --save
+```
+## Modules
+### Logger
+This class is responsible for application logging
+## Test
+```sh
+npm run test
+```
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,26 +1,58 @@
 import { BehaviorSubject } from 'rxjs';
 import * as moment from 'moment';
 
+/**
+ * This class responsible for allication logging
+ */
 export class Logger {
+  /**
+   * Application logger level
+   */
   static level: Logger.Level;
+  /**
+   * Application name, added to logger output
+   */
   static appName: string = '';
+  /**
+   * Application version, added to logger output
+   */
   static appVersion: string = '';
+  /**
+   * Filter modules to log
+   */
   static moduleNames: string[] = [];
 
+  /**
+   * On log event
+   */
   static onLog: BehaviorSubject<Logger.Log> = new BehaviorSubject(null);
 
+  /**
+   * Creates logger instance with module name.
+   * @param name {string} module name
+   */
   constructor(private name: string) {
     Logger.level = Logger.Level.DEBUG;
   }
 
+  /**
+   * Outputs logs with info level
+   * @param params
+   */
   info(...params) {
     this.filterLog(Logger.Level.INFO, params);
   }
-
+  /**
+   * Outputs logs with debug level
+   * @param params
+   */
   debug(...params) {
     this.filterLog(Logger.Level.DEBUG, params);
   }
-
+  /**
+   * Outputs logs with error level
+   * @param params
+   */
   error(...params) {
     this.filterLog(Logger.Level.ERROR, params);
   }
@@ -31,12 +63,20 @@ export class Logger {
     }
   }
 
+  /**
+   * Outputs logs
+   * @param appName {string} application name
+   * @param appVersion {string} application version
+   * @param moduleName {string} module name
+   * @param level {number} log level
+   * @param params
+   */
   log(appName: string, appVersion: string, moduleName: string, level: number, params: any[]) {
-    params = params.map(param => {
+    const normalizedParams = params.map(param => {
       try {
         if (param && typeof param === 'object') {
           if (param.message && param.stack) {
-            return param.message + ', stack: ' + param.stack;
+            return `${param.message}, stack: ${param.stack}`;
           } else {
             return JSON.stringify(param);
           }
@@ -47,29 +87,34 @@ export class Logger {
         return err;
       }
     });
-    const date = new Date();
+    const date = Date.now();
     const prefixStr = `[${moment(date).format('HH:mm:ss.SSS')} ${appName}.${moduleName}]`;
-    var method = level === Logger.Level.ERROR ? console.error : console.log;
-    method.apply(console, [prefixStr, ...params]);
+    const method = level === Logger.Level.ERROR ? console.error : console.log;
+    method.apply(console, [prefixStr, ...normalizedParams]);
     Logger.onLog.next({
-      date: date.getTime(),
+      date,
       level,
       appName,
       appVersion,
       moduleName,
-      params,
+      params: normalizedParams,
     });
   }
-
 }
 
 export namespace Logger {
+  /**
+   * Logging level
+   */
   export enum Level {
     INFO = 2,
     DEBUG = 1,
-    ERROR = 0
-  };
+    ERROR = 0,
+  }
 
+  /**
+   * Log entry model
+   */
   export interface Log {
     date: number;
     level: Logger.Level;
